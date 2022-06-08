053de9667fd5e50bd5da23bbeea8a7f96603e850
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-08 12:53:40 -0400

    Added time conversion utils

---
+++ src/utils/timeConversions.jsx
@@ -0,0 +1,7 @@
+// Epoch timestamp to which day of the week 
+export const epochConversion = (timestamp) => {
+    const time = new Date(timestamp*1000);
+    const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const day = days[time.getDay()];
+    return day;
+}
